b3616d550dc02813cfa6ec0300c3e1f2887bc0b7
Joshua Devon <joshua.devon@gmail.com>
2024-05-01 12:04:39 +1000

    Completed excercise 10

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titles = [];
+    for (let i = 0; i < array.length; i++) {
+        titles.push(array[i].title);
+    }
+    return titles;
 };
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+  console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
