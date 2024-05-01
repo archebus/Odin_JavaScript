4572055246afced7ec02c7638eabd87312235c8f
Joshua Devon <joshua.devon@gmail.com>
2024-05-01 12:00:22 +1000

    Completed excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(end) {
+    let fib = 0;
+    let add = 1;
+    let next = 0;
 
+    if (end < 0) {
+        return "OOPS";
+    }
+    
+    for (let i = 1; i <= end; i++){
+        next = fib + add;
+        fib = add;
+        add = next;
+    }
+    return fib;
 };
 
 // Do not edit below this line
