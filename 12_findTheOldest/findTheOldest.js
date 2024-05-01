e84369162757674c04384132050c16f7b69e1f6d
Joshua Devon <joshua.devon@gmail.com>
2024-05-01 12:29:57 +1000

    Completed all excersises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,49 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let oldestIndex = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].yearOfDeath == undefined) {
+            array[i].yearOfDeath = getYear();
+        }
+    }
+
+    let ages = array.map(
+        (array) => (array.yearOfDeath - array.yearOfBirth)
+    );
+
+    for (let i = 0; i < ages.length; i++) {
+        if (ages[i] > ages[oldestIndex]){
+            oldestIndex = i;
+        }
+    }
+    return array[oldestIndex];
+
+    function getYear() {
+        const d = new Date();
+        return d.getFullYear();
+    }
 
 };
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+
+  console.log(findTheOldest(people).name);
 
 // Do not edit below this line
 module.exports = findTheOldest;
