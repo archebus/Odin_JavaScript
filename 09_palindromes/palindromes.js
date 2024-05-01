c79b2e7ed6e8723710ffa190d03b287d3a54bbdf
Joshua Devon <joshua.devon@gmail.com>
2024-05-01 11:42:24 +1000

    Completed excersie 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    var punctuation = /[\.,?! ]/g;
+    var cleanString = string.replace(punctuation, "").toLowerCase();
+    const array = cleanString.split('');
+    const rArray = array.toReversed();
 
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] != rArray[i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
