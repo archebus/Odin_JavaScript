9c75a24cd569315a4f6ffca9ba2ecf48a8bdea0a
Joshua Devon <joshua.devon@gmail.com>
2024-05-01 10:57:34 +1000

    Completed Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length == 0) {
+    return 0;
+  }
+  let startSum = 0;
+  let reducedSum = array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    startSum,
+  );
+  return reducedSum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if (array.length == 0) {
+    console.log("if true");
+    return 0;
+  }
+  let multipliedSum = array.reduce(
+    (accumulator, currentValue) => accumulator * currentValue
+  );
+  console.log(multipliedSum);
+  return multipliedSum;
 };
 
-const power = function() {
-	
+
+const power = function(a,b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a < 0) return -1;
+  else if (a == 0) return 1;
+  else {
+    return (a * factorial(a - 1));
+  };
 };
 
 // Do not edit below this line
